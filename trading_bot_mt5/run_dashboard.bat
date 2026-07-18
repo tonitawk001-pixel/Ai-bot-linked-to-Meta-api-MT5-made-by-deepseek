94d4056045ce04142d470b0ef379871d493e9a88
tonitawk001-pixel <toni.tawk001@gmail.com>
2026-07-19 01:56:51 +0300

    Remove trading_bot_mt5 folder - this repo is now MetaApi-only

--- trading_bot_mt5/run_dashboard.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting V22 Bot Dashboard...
-echo Open: http://localhost:5000
-echo Close this window to stop the dashboard.
-echo.
-python dashboard_simple.py
-pause
